a23d68c0c4cb6850a85a3c999e5a4a8f8fc2e315
Migueldaw707 <105781717+Migueldaw707@users.noreply.github.com>
2022-05-22 23:02:00 +0200

    Primer commit

---
+++ src/gitjav/GitJav.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitjav;
+
+/**
+ *
+ * @author miguel 6K
+ */
+public class GitJav {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a= 2, b= 3;
+        int c= a+b;
+        System.out.println("El resultado es: "+c);
+        
+    }
+    
+}
