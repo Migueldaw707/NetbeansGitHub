9441cefc0b649e20e163b07d793427e7c50237b1
Migueldaw707 <105781717+Migueldaw707@users.noreply.github.com>
2022-05-22 23:17:42 +0200

    Revert "segundo commit"
    This reverts commit e5c56c94e9f2b900350650d968882c176edc958b.

--- src/gitjav/GitJav.java
+++ src/gitjav/GitJav.java
@@ -14,7 +14,7 @@ public class GitJav {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a= 2, b= 4;
+        int a= 2, b= 3;
         int c= a+b;
         System.out.println("El resultado es: "+c);
         
