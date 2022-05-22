b898bc2528d761411baa5729430f4108234067d0
Migueldaw707 <105781717+Migueldaw707@users.noreply.github.com>
2022-05-23 00:29:53 +0200

    cambios en copia

---
+++ copiagit/src/copiagit/Copiagit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package copiagit;
+
+/**
+ *
+ * @author miguel 6K
+ */
+public class Copiagit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a= 3, b= 4;
+        int c= a+b;
+        System.out.println("El resultado es: "+c);
+    }
+    
+}
